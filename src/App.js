3c3c5aa6f24115d8fed8e93264ecf107fd7ace90
tonyg <aj.gianno@gmail.com>
2021-04-24 14:25:46 -0500

    buttonGroup

--- src/App.js
+++ src/App.js
@@ -1,20 +1,34 @@
 import React from 'react';
 import Button from '@material-ui/core/Button';
+import ButtonGroup from '@material-ui/core/ButtonGroup';
 import SaveIcon from '@material-ui/icons/Save';
 import './App.css';
 
 function App() {
   return (
     <div className='App'>
-      <Button
-        startIcon={<SaveIcon />}
-        style={{ fontSize: 24 }}
-        onClick={() => alert('hello')}
-        variant='contained'
-        color='primary'
-      >
-        Hello, Button
-      </Button>
+      <ButtonGroup>
+        <Button
+          startIcon={<SaveIcon />}
+          endIcon={<SaveIcon />}
+          style={{ fontSize: 24 }}
+          onClick={() => alert('hello')}
+          variant='contained'
+          color='primary'
+        >
+          Save, Button
+        </Button>
+        <Button
+          startIcon={<SaveIcon />}
+          endIcon={<SaveIcon />}
+          style={{ fontSize: 24 }}
+          onClick={() => alert('hello')}
+          variant='contained'
+          color='primary'
+        >
+          Discard, Button
+        </Button>
+      </ButtonGroup>
     </div>
   );
 }
