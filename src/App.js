72d701d1855960f65bcfd02e0b3ca96eb9594921
tonyg <aj.gianno@gmail.com>
2021-04-24 14:40:23 -0500

    formControlLabel for checkboxes

--- src/App.js
+++ src/App.js
@@ -2,29 +2,48 @@ import React from 'react';
 import Button from '@material-ui/core/Button';
 import ButtonGroup from '@material-ui/core/ButtonGroup';
 import SaveIcon from '@material-ui/icons/Save';
+import DeleteIcon from '@material-ui/icons/Delete';
+import Checkbox from '@material-ui/core/Checkbox';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+
 import './App.css';
+
+function CheckboxExample() {
+  const [checked, setChecked] = React.useState(true);
+  return (
+    <FormControlLabel
+      control={
+        <Checkbox
+          checked={checked}
+          onChange={(e) => setChecked(e.target.checked)}
+          inputProps={{ 'aria-label': 'secondary checkbox' }}
+        />
+      }
+    />
+  );
+}
 
 function App() {
   return (
     <div className='App'>
-      <ButtonGroup>
+      <ButtonGroup variant='contained' color='primary'>
+        <CheckboxExample />
+        <CheckboxExample />
+        <CheckboxExample />
+        <CheckboxExample />
+        <br />
         <Button
           startIcon={<SaveIcon />}
-          endIcon={<SaveIcon />}
           style={{ fontSize: 24 }}
-          onClick={() => alert('hello')}
-          variant='contained'
-          color='primary'
+          onClick={() => alert('Saved')}
         >
           Save, Button
         </Button>
         <Button
-          startIcon={<SaveIcon />}
-          endIcon={<SaveIcon />}
+          endIcon={<DeleteIcon />}
           style={{ fontSize: 24 }}
-          onClick={() => alert('hello')}
-          variant='contained'
-          color='primary'
+          onClick={() => alert('Deleted')}
+          color='secondary'
         >
           Discard, Button
         </Button>
