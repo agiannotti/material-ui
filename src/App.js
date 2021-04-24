25b96ba35387e9bd6dd6c95f4260d9e41b8bec09
tonyg <aj.gianno@gmail.com>
2021-04-24 15:16:44 -0500

    applying styled themes

--- src/App.js
+++ src/App.js
@@ -7,7 +7,41 @@ import Checkbox from '@material-ui/core/Checkbox';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import TextField from '@material-ui/core/TextField';
 
+import {
+  makeStyles,
+  ThemeProvider,
+  createMuiTheme,
+} from '@material-ui/core/styles';
+
 import './App.css';
+import { grey, purple } from '@material-ui/core/colors';
+
+const useStyles = makeStyles({
+  root: {
+    background: 'linear-gradient(45deg, #FE6b8b, #FF8E53)',
+    border: 0,
+    borderRadius: 15,
+    marginBottom: 14,
+    color: 'white',
+    padding: '15px 30px',
+  },
+});
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: grey[200],
+    },
+    secondary: {
+      main: purple[200],
+    },
+  },
+});
+
+function ButtonStyled() {
+  const classes = useStyles();
+  return <Button className={classes.root}>Test Button!</Button>;
+}
 
 function CheckboxExample() {
   const [checked, setChecked] = React.useState(true);
@@ -29,39 +63,42 @@ function CheckboxExample() {
 
 function App() {
   return (
-    <div className='App'>
-      <TextField
-        variant='filled'
-        type='email'
-        placeholder='meow@nyan.com'
-        label='email'
-      />
-      <TextField variant='outlined' type='time' label='TIME' />
-      <TextField variant='outlined' color='secondary' type='date' />
+    <ThemeProvider theme={theme}>
+      <div className='App'>
+        <ButtonStyled />
+        <TextField
+          variant='filled'
+          type='email'
+          placeholder='meow@nyan.com'
+          label='email'
+        />
+        <TextField variant='outlined' type='time' label='TIME' />
+        <TextField variant='outlined' color='secondary' type='date' />
 
-      <CheckboxExample />
-      <CheckboxExample />
-      <CheckboxExample />
-      <CheckboxExample />
-      <ButtonGroup variant='contained' color='primary'>
-        <br />
-        <Button
-          startIcon={<SaveIcon />}
-          style={{ fontSize: 24 }}
-          onClick={() => alert('Saved')}
-        >
-          Save Button
-        </Button>
-        <Button
-          endIcon={<DeleteIcon />}
-          style={{ fontSize: 24 }}
-          onClick={() => alert('Deleted')}
-          color='secondary'
-        >
-          Discard Button
-        </Button>
-      </ButtonGroup>
-    </div>
+        <CheckboxExample />
+        <CheckboxExample />
+        <CheckboxExample />
+        <CheckboxExample />
+        <ButtonGroup variant='contained' color='primary'>
+          <br />
+          <Button
+            startIcon={<SaveIcon />}
+            style={{ fontSize: 24 }}
+            onClick={() => alert('Saved')}
+          >
+            Save Button
+          </Button>
+          <Button
+            endIcon={<DeleteIcon />}
+            style={{ fontSize: 24 }}
+            onClick={() => alert('Deleted')}
+            color='secondary'
+          >
+            Discard Button
+          </Button>
+        </ButtonGroup>
+      </div>
+    </ThemeProvider>
   );
 }
 
